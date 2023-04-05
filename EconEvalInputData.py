ca9c12eb1c0b462fe4595d1bf91df90904b2969f
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2023-04-05 16:26:49 -0400

    2023

--- EconEvalInputData.py
+++ EconEvalInputData.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 # simulation settings
-POP_SIZE = 1000     # cohort population size
+POP_SIZE = 2000     # cohort population size
 SIM_LENGTH = 1000   # length of simulation (years)
 ALPHA = 0.05        # significance level for calculating confidence intervals
 DISCOUNT = 0.03     # annual discount rate
